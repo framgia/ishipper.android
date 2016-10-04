773cb9c976d6fe3dfea0377df722d50744e93296
Duy Chuong Vu <vu.duy.chuong@framgia.com>
2016-10-04 09:14:46 +0700

    fix setting ui

--- app/src/main/java/com/framgia/ishipper/ui/fragment/UserInfoDialogFragment.java
+++ app/src/main/java/com/framgia/ishipper/ui/fragment/UserInfoDialogFragment.java
@@ -1,6 +1,8 @@
 package com.framgia.ishipper.ui.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
@@ -9,6 +11,7 @@ import android.support.v7.widget.AppCompatRatingBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.framgia.ishipper.R;
@@ -56,6 +59,14 @@ public class UserInfoDialogFragment extends DialogFragment {
         ButterKnife.bind(this, view);
         initView();
         return view;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
     }
 
     private void initView() {
