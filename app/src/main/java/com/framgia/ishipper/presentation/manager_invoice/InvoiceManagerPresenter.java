bebbbfdc6c8b64bbe08b0bae2b46f28b296f7108
Duy Chuong Vu <vu.duy.chuong@framgia.com>
2016-11-23 14:20:38 +0700

    refactor invoice manager fragment

---
+++ app/src/main/java/com/framgia/ishipper/presentation/manager_invoice/InvoiceManagerPresenter.java
@@ -0,0 +1,108 @@
+package com.framgia.ishipper.presentation.manager_invoice;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.framgia.ishipper.base.BaseActivity;
+import com.framgia.ishipper.common.Config;
+import com.framgia.ishipper.model.Invoice;
+import com.framgia.ishipper.model.User;
+import com.framgia.ishipper.net.API;
+import com.framgia.ishipper.net.APIResponse;
+import com.framgia.ishipper.net.data.InvoiceData;
+import com.framgia.ishipper.ui.activity.ListShipperRegActivity;
+import com.framgia.ishipper.ui.activity.OrderDetailActivity;
+import com.framgia.ishipper.util.CommonUtils;
+
+/**
+ * Created by vuduychuong1994 on 11/23/16.
+ */
+
+public class InvoiceManagerPresenter implements InvoiceManagerContract.Presenter {
+
+    private BaseActivity mActivity;
+    private InvoiceManagerContract.View mView;
+
+    public InvoiceManagerPresenter(
+            BaseActivity activity, InvoiceManagerContract.View view) {
+        mActivity = activity;
+        mView = view;
+    }
+
+    @Override
+    public void actionTakeInvoice(final Invoice invoice) {
+        mActivity.showDialog();
+        API.putUpdateInvoiceStatus(User.ROLE_SHIPPER, String.valueOf(invoice.getId()),
+                                   Config.getInstance().getUserInfo(mActivity).getAuthenticationToken(),
+                                   Invoice.STATUS_SHIPPING, new API.APICallback<APIResponse<InvoiceData>>() {
+                    @Override
+                    public void onResponse(APIResponse<InvoiceData> response) {
+                        mView.notifyChangeTab(Invoice.STATUS_CODE_WAITING);
+                        mView.notifyChangeTab(Invoice.STATUS_CODE_SHIPPING, true, invoice.getId());
+                        mActivity.dismissDialog();
+                    }
+
+                    @Override
+                    public void onFailure(int code, String message) {
+                        mActivity.dismissDialog();
+                        Toast.makeText(mActivity, message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void actionShippedInvoice(final Invoice invoice) {
+        API.putUpdateInvoiceStatus(User.ROLE_SHIPPER, String.valueOf(invoice.getId()),
+                                   Config.getInstance().getUserInfo(mActivity).getAuthenticationToken(),
+                                   Invoice.STATUS_SHIPPED, new API.APICallback<APIResponse<InvoiceData>>() {
+                    @Override
+                    public void onResponse(APIResponse<InvoiceData> response) {
+                        mView.notifyChangeTab(Invoice.STATUS_CODE_SHIPPING);
+                        mView.notifyChangeTab(Invoice.STATUS_CODE_SHIPPED, true, invoice.getId());
+                        mView.showReviewDialog(invoice);
+                    }
+
+                    @Override
+                    public void onFailure(int code, String message) {
+                        Toast.makeText(mActivity, message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void actionFinishInvoice(final Invoice invoice) {
+        API.putUpdateInvoiceStatus(User.ROLE_SHOP, String.valueOf(invoice.getId()),
+                                   Config.getInstance().getUserInfo(mActivity).getAuthenticationToken(),
+                                   Invoice.STATUS_FINISHED, new API.APICallback<APIResponse<InvoiceData>>() {
+                    @Override
+                    public void onResponse(APIResponse<InvoiceData> response) {
+                        mView.notifyChangeTab(Invoice.STATUS_CODE_SHIPPED);
+                        mView.notifyChangeTab(Invoice.STATUS_CODE_FINISHED, true, invoice.getId());
+                        mView.showReviewDialog(invoice);
+                    }
+
+                    @Override
+                    public void onFailure(int code, String message) {
+                        Toast.makeText(mActivity, message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void startDetailInvoiceActivity(Invoice invoice) {
+        Intent intent = new Intent(mActivity, OrderDetailActivity.class);
+        Bundle extras = new Bundle();
+        extras.putInt(OrderDetailActivity.KEY_INVOICE_ID, invoice.getId());
+        intent.putExtras(extras);
+        mActivity.startActivityForResult(intent,
+                                         OrderDetailActivity.REQUEST_INVOICE_ID);
+    }
+
+    @Override
+    public void startListShipperRegActivity(Invoice invoice) {
+        Intent intent = new Intent(mActivity, ListShipperRegActivity.class);
+        intent.putExtra(ListShipperRegActivity.KEY_INVOICE_ID, invoice.getId());
+        mActivity.startActivityForResult(intent,
+                                         ListShipperRegActivity.REQUEST_CODE_RESULT);
+    }
+}
